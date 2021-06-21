79e8367ad48e0f8a79aedeae4bd62c1b13315401
Robert Jeffrey <iamrobertjeffrey@gmail.com>
2021-06-21 16:46:50 +0200

    completedTwoSum

---
+++ twoSum/app.js
@@ -0,0 +1,18 @@
+function twoSum(nums, target) 
+{ 
+    for(i = 0; i < nums.length; i++)
+        {
+            for(j = i + 1; j < nums.length; j++)
+                {
+                    const currentSum = nums[i] + nums[j];  
+                    
+                    if(currentSum === target)
+                        {
+                            const answerArray = [];
+                            answerArray.push(i);
+                            answerArray.push(j);
+                            return answerArray;
+                        }
+                }
+        }
+};
